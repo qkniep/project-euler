fce46441455865ff7eba392a9eed65a82514c7c9
Quentin Kniep <hello@quentinkniep.com>
2020-12-07 19:51:58 +0100

    ✨ solve problem 35

---
+++ problem035.hs
@@ -0,0 +1,16 @@
+#! /usr/bin/env runghc
+
+-- Copyright (C) 2020 Quentin M. Kniep <hello@quentinkniep.com>
+-- Distributed under terms of the MIT license.
+
+-- this one takes around 2 minutes
+
+import Data.List.Ordered (has, minus, unionAll)
+
+main = putStrLn (show (filter isCirc primesBelowMill))
+primesBelowMill = takeWhile (\p -> p < 1000000) primes
+primes = 2 : 3 : minus [5,7..] (unionAll [[p*p, p*p+2*p..] | p <- tail primes])
+isCirc p = all (\x -> has primesBelowMill x) (rotations p p)
+rotations a b
+  | a < 10 = [b]
+  | otherwise = b : rotations (div a 10) ((div b 10) + 10 ^ (length (show b) - 1) * (mod b 10))
