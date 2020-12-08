070eba8098c73ce517c7a6e0c83e92550b1ef968
Quentin Kniep <hello@quentinkniep.com>
2020-12-08 11:31:27 +0100

    ⚡️ make problem 35 six times faster
    Achieved this by only considering numbers made of odd digits only.

--- problem035.hs
+++ problem035.hs
@@ -3,14 +3,19 @@
 -- Copyright (C) 2020 Quentin M. Kniep <hello@quentinkniep.com>
 -- Distributed under terms of the MIT license.
 
--- this one takes around 2 minutes
+-- this one takes about 20 seconds
 
 import Data.List.Ordered (has, minus, unionAll)
+import Data.Set (fromList, deleteFindMin, insert)
 
-main = putStrLn (show (length (filter isCirc primesBelowMill)))
+main = putStrLn (show (length (filter isCirc (takeWhile (<1000000) numbers))))
 primesBelowMill = takeWhile (<1000000) primes
 primes = 2 : 3 : minus [5,7..] (unionAll [[p*p, p*p+2*p..] | p <- tail primes])
 isCirc p = all (has primesBelowMill) (rotations p p)
+
+numbers = 2 : f (fromList [1,3..9]) where
+f s = m : f (foldl (flip insert) s' $ map (10*m +) [1,3..9])
+     where (m, s') = deleteFindMin s
 
 rotations a b
   | a < 10 = [b]
