ae35ed3f78a73f32c9b5840a0a76683ea6f2fa5e
Quentin Kniep <hello@quentinkniep.com>
2021-02-01 16:35:00 +0100

    ✨ solve problem 114

---
+++ problem114.hs
@@ -0,0 +1,16 @@
+#! /usr/bin/env runghc
+
+-- Copyright (C) 2020 Quentin M. Kniep <hello@quentinkniep.com>
+-- Distributed under terms of the MIT license.
+
+main = putStrLn (show (combinations 50))
+
+combinations = (map blockCombinations [0..] !!)
+
+blockCombinations n
+  | n < 3 = 1
+  | otherwise = 1 + sum (map (\s -> sum (map (\p -> safeLookup combinations (n-s-p-1)) [0..n-s])) [3..n])
+
+safeLookup l i
+  | i < 0 = 1
+  | otherwise = l i
